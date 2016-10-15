be0ad63a815e18cc9382f5a6453effd6bf3b8256
ahyanarizky <ahyana.rizky@gmail.com>
2016-10-15 19:25:55 +0700

    CRUD + migration table with sequelize & postgres done

---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const models = require('./models')
+let user = models.User
+// user.add('Syanmil', '1993-02-29', 'my name is syanmil')
+
+// user.editName('ahyana', 1)
+
+user.see()
+// user.destroy(2)
